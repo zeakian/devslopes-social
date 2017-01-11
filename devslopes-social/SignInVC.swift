ec0e0dd24195fbc61992c030677b99ac4c5824e2
zeakian <zeakian@gmail.com>
2017-01-10 21:25:11 -0500

    initial commit

---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devslopes-social
+//
+//  Created by zeakian on 1/10/17.
+//  Copyright © 2017 zeakian. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
